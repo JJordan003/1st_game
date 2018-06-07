84a0091fe2cc9cadd80d25fca46388c2ca899da7
pikol93 <pikol93@users.noreply.github.com>
2018-06-07 18:15:04 +0200

    Dodanie sceny
    Dodanie sceny i poprawienie rozdzielczosci obrazow

--- scenes/background.gd
+++ scenes/background.gd
@@ -1,4 +1,4 @@
-extends TextureButton
+extends Sprite
 
 # class member variables go here, for example:
 # var a = 2
@@ -7,12 +7,10 @@ extends TextureButton
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	
 	pass
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
-func _pressed():
-	get_tree().change_scene("res://scenes/loading.tscn")
-	pass
--- scenes/node.gd
+++ scenes/node.gd
@@ -1,4 +1,4 @@
-extends TextureButton
+extends Node2D
 
 # class member variables go here, for example:
 # var a = 2
@@ -13,6 +13,3 @@ func _ready():
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
-func _pressed():
-	get_tree().change_scene("res://scenes/loading.tscn")
-	pass
