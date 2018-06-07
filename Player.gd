beea5a0255938ac9652940855dd1046993101b2a
pikol93 <pikol93@users.noreply.github.com>
2018-06-07 21:29:40 +0200

    Dodanie 3 sceny (main)
    +dodanie sceny main
    +dodanie kolizji do postaci
    +dodanie podstawowego modelu do postaci (circle.png)
    *zmiana nazw (sciezek) do plikow graficznych

---
+++ Player.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+export (int) var SPEED  # how fast the player will move (pixels/sec)
+var screensize  # size of the game window
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
