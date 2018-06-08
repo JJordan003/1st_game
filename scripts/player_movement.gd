8ce68b907c9a0259ff045166bedeb89185013a50
pikol93 <pikol93@users.noreply.github.com>
2018-06-08 22:01:05 +0200

    Prototyp skakania (?)
    Dodano mozliwosc poruszania sie po osi y poprzez wcisniecie przycisku spacji

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -2,12 +2,18 @@ extends Area2D
 
 export (float) var SPEED
 var screensize
+var prop_gravity = 0
+var ispressed = 0
+const MAX_ACCELERATION = 100
+const MIN_ACCELERATION = -100
+var space_cooldown = 0
 
 func _ready():
 	screensize = get_viewport_rect().size
 	pass
 
 func _process(delta):
+	
 	var velocity = Vector2()
 	if Input.is_key_pressed(KEY_A):
 		velocity.x -= 1
@@ -19,11 +25,21 @@ func _process(delta):
 		velocity.y += 1
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED
+		
+	if Input.is_key_pressed("KEY_SPACE") and ispressed == 0:
+		prop_gravity = clamp(-1, -1, 1)
+		print("SPACE")
+		ispressed = 1
+	if !Input.is_key_pressed("KEY_SPACE") and ispressed == 1:
+		ispressed = 0
+	
+	if prop_gravity > 0:
+		prop_gravity -= 0.1
+	if prop_gravity < 0:
+		prop_gravity += 0.1
+		
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screensize.x)
 	position.y = clamp(position.y, 0, screensize.y)
-	if Input.is_key_pressed(KEY_K):
-		print(position.x,", ",0,", ",screensize.x)
-		print(position.y,", ",0,", ",screensize.y)
 	pass
 	
