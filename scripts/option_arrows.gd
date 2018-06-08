f4a57a9715c119370c68159193566a4134d4f4aa
JJordan003 <34778792+JJordan003@users.noreply.github.com>
2018-06-09 00:45:35 +0200

    ShadowFix
    zakomentowano skrpyt strzałki

--- scripts/option_arrows.gd
+++ scripts/option_arrows.gd
@@ -3,7 +3,6 @@ extends TextureButton
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -13,9 +12,10 @@ func _ready():
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
-func _pressed():
-	if position.x == get_parent().get_child(1):
-		var x = true #placeholder code
-	elif position.x == get_parent().get_child(2):
-		var y = true #placeholder code
-	pass
+#func _pressed():
+#	var x = position.x
+#	if x == get_parent().get_child(1).position.x:
+#		var a = true #placeholder code
+#	elif x == get_parent().get_child(2).position.x:
+#		var b = true #placeholder code
+#	pass
