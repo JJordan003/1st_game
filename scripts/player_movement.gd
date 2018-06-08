17f0d7bf5744a3ced25980e4a0b93f913fd11807
pikol93 <pikol93@users.noreply.github.com>
2018-06-08 16:10:20 +0200

    v0.0.1
    +dodano ruch do postaci
    +nowa zmienna SPEED okreslajaca maksymalna predkosc postaci
    +zmiana domyslej rozdzielczosci z 1920x1080 na 1600x900
    +wylaczenie opcji rozciagania okna

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,14 +1,29 @@
 extends Area2D
 
-export (int) var SPEED  # how fast the player will move (pixels/sec)
-var screensize  # size of the game window
+export (float) var SPEED = 200
+var screensize
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+	screensize = get_viewport_rect().size
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_key_pressed(KEY_A):
+		velocity.x -= 1
+	if Input.is_key_pressed(KEY_D):
+		velocity.x += 1
+	if Input.is_key_pressed(KEY_W):
+		velocity.y -= 1
+	if Input.is_key_pressed(KEY_S):
+		velocity.y += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * SPEED
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+	if Input.is_key_pressed(KEY_K):
+		print(position.x,", ",0,", ",screensize.x)
+		print(position.y,", ",0,", ",screensize.y)
+	pass
+	
