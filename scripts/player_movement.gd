a8b6e3311193ecd82cc62e555f9382b89fc02e34
pikol93 <pikol93@users.noreply.github.com>
2018-06-09 00:40:56 +0200

    v0.0.2a
    +dodano grawitacje
    +dodano mozliwosc skakania
    +zmieniono granice poziomu na main.tscn

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,15 +1,19 @@
 extends Area2D
 
-export (float) var SPEED
+export (int) var SPEED
+export (int) var JUMP_HEIGHT_REVERSED = 20
+export (int) var GRAVITY = 25
 var screensize
 var prop_gravity = 0
 var ispressed = false
-const MAX_ACCELERATION = 100
-const MIN_ACCELERATION = -100
+var acceleration = 0
 var space_cooldown = 0
+var jump_height = -JUMP_HEIGHT_REVERSED #zmiana znaku przy exportowanej wartosci
 
 func _ready():
 	screensize = get_viewport_rect().size
+	var ready_velocity = Vector2()
+	ready_velocity.y = 0.1
 	pass
 
 func _process(delta):
@@ -19,27 +23,21 @@ func _process(delta):
 		velocity.x -= 1
 	if Input.is_key_pressed(KEY_D):
 		velocity.x += 1
-	if Input.is_key_pressed(KEY_W):
-		velocity.y -= 1
-	if Input.is_key_pressed(KEY_S):
-		velocity.y += 1
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED
-		
-	if Input.is_key_pressed("KEY_SPACE") and ispressed == false:
-#		prop_gravity = clamp(-1, -1, 1)
-		print("SPACE")
+	if Input.is_key_pressed(KEY_SPACE) and ispressed == false:
+		acceleration = jump_height
 		ispressed = true
-	if !Input.is_key_pressed("KEY_SPACE") and ispressed == 1:
-		ispressed = 0
+	if !Input.is_key_pressed(KEY_SPACE) and ispressed == true:
+		ispressed = false
 	
-	if prop_gravity > 0:
-		prop_gravity -= 0.1
-	if prop_gravity < 0:
-		prop_gravity += 0.1
-		
+	velocity.y += acceleration * GRAVITY
+	if acceleration < 20:
+		if velocity.y != 0:
+			acceleration += 0.511111
+	
 	position += velocity * delta
-	position.x = clamp(position.x, 0, screensize.x)
-	position.y = clamp(position.y, 0, screensize.y)
+	position.x = clamp(position.x, 0+(get_child(0).scale.x*20), screensize.x-(get_child(0).scale.x*20))
+	position.y = clamp(position.y, 0+(get_child(0).scale.y*20), screensize.y-(get_child(0).scale.y*20))
 	pass
 	
